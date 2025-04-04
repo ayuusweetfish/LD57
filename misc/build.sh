9e1329db3dec518704f5982cfdf72d9dbe06e520
Ayu <claire@ayu.land>
2025-04-05 00:25:13 +0800

    Revise build script

--- misc/build.sh
+++ misc/build.sh
@@ -2,12 +2,14 @@ BOON=~/Downloads/boon-macos-amd64/boon
 NODE=~/Downloads/node-v20.12.2-darwin-x64/bin/node
 LOVEJS_INDEX=~/Downloads/lovejs-11/node_modules/love.js/index.js
 
-${BOON} build . --target love
+TARGET=${1:-all}
+
+${BOON} build . --target ${TARGET}
 
 # Web
 rm -rf release/Game-Name-web
 ${NODE} ${LOVEJS_INDEX} -c -t "Game Name" -m 64000000 "release/Game Name.love" release/Game-Name-web
 cp misc/web_index.html release/Game-Name-web/index.html
+# Patch for filesystem access
+sed -i '' 's/var SYSCALLS/try{if(!window.FS)window.FS=FS;}catch(e){}var SYSCALLS/' release/Game-Name-web/love.js
 rm -rf release/Game-Name-web/theme
-
-# (cd release/*-web; python3 -m http.server)

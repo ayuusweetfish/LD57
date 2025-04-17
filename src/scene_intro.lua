437311805d0775ad1984efae29f8254aae9aee8b
Ayu <claire@ayu.land>
2025-04-17 16:18:28 +0800

    Add intro scene

--- src/scene_intro.lua
+++ src/scene_intro.lua
@@ -1,48 +1,63 @@
 local draw = require 'draw_utils'
 local button = require 'button'
+local audio = require 'audio'
 
 return function ()
   local s = {}
   local W, H = W, H
-  local font = _G['global_font']
 
-  local t1 = love.graphics.newText(font(80), 'Game')
+  local T = 0
+  local since_press = -1
 
-  local btn = button(
-    draw.enclose(love.graphics.newText(font(36), 'Start'), 120, 60),
-    function () replaceScene(scene_intro(), transitions['fade'](0.1, 0.1, 0.1)) end
-  )
-  btn.x = W * 0.5
-  btn.y = H * 0.65
-  local buttons = { btn }
+  local start_game = function ()
+    if since_press < 0 then
+      audio.sfx('ad_astra_morse')
+      since_press = 0
+    end
+  end
 
   s.press = function (x, y)
-    for i = 1, #buttons do if buttons[i].press(x, y) then return true end end
   end
 
   s.hover = function (x, y)
   end
 
   s.move = function (x, y)
-    for i = 1, #buttons do if buttons[i].move(x, y) then return true end end
   end
 
   s.release = function (x, y)
-    for i = 1, #buttons do if buttons[i].release(x, y) then return true end end
+    start_game()
+  end
+
+  s.key = function (key)
+    if key == 'return' then start_game() end
   end
 
   s.update = function ()
-    for i = 1, #buttons do buttons[i].update() end
+    T = T + 1
+    if since_press >= 0 then
+      since_press = since_press + 1
+      if since_press == 480 then
+        replaceScene(scene_gameplay(1), transitions['crossfade']())
+      end
+    end
   end
 
   s.draw = function ()
-    love.graphics.clear(0.996, 0.988, 0.878)
-    love.graphics.setColor(1, 1, 1)
-    draw.img('intro_bg', W / 2, H / 2, W, H)
-    draw.shadow(0.1, 0.1, 0.1, 1, t1, W / 2, H * 0.35)
+    love.graphics.clear(0, 0, 0)
 
-    love.graphics.setColor(0.1, 0.1, 0.1)
-    for i = 1, #buttons do buttons[i].draw() end
+    love.graphics.setColor(1, 1, 1)
+    draw.img('ending/bg_int', W / 2, H / 2, W, H)
+    love.graphics.setColor(0.22, 0.38, 0.22)
+    local noise_frame = 1 + math.floor(T / 20) % 10
+    draw.img('noise/' .. noise_frame, W / 2, (111 + 630/2) * (2/3))
+
+    love.graphics.setColor(1, 1, 1)
+    draw.img('intro/bg', W / 2, H / 2, W, H)
+
+    local progress = math.max(0, math.min(1, since_press / 400))
+    local delta_y = progress * progress * progress
+    draw.img('intro/fg', W / 2, 460 / 720 * H + H * 0.62 * delta_y)
   end
 
   s.destroy = function ()
